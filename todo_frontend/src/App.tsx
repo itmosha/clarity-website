5886e4bea4cee84310db5fc4565e5660ce65433c
itmosha <itmosha0@gmail.com>
2023-02-09 11:01:13 +0300

    changed: submodule fix

---
+++ todo_frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+function App() {
+    return (
+        <Box>
+            <Heading>This is homepage</Heading>
+        </Box>
+    );
+}
+
+export default App;
