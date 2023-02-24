165cd7fe5a55f14d0de46eb790900f9e3c5a554a
itmosha <itmosha0@gmail.com>
2023-02-25 00:27:49 +0300

    changed: new design applied

--- todo_frontend/src/IndexPage.tsx
+++ todo_frontend/src/IndexPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Box, Heading, Text, Flex, VStack, Center} from '@chakra-ui/react';
+import {Box, Heading, Text, Flex, VStack, Center, Button, Link, Image} from '@chakra-ui/react';
 import Navbar from "./components/Navbar";
 import IndexLinksBar from "./components/IndexLinksBar";
-
+import { motion } from 'framer-motion';
 // BG #161920 
 // ITEMS #1C1F27
 
@@ -10,74 +10,62 @@ function IndexPage() {
     return (
         <Box minH={'100vh'} w={'100vw'} bgColor={'#161920'}>
             <Navbar />
-            <Box px={'7vw'}>
-                <Box>
-                    <Heading>Here is all text</Heading>
-                </Box>
-                <Box bgColor={'#1C1F27'} h='300px' w='86vw' boxShadow={'0px 0px 30px 10px rgba(0, 0, 0, .2)'}>
-
-                </Box>
-            </Box>
-        {/* <Navbar /> */}
-            {/* <Flex p={'5vh 5vw'} justify={'space-between'}> */}
-                {/* <VStack align={'start'}>
-                    <Flex>
-                        <Heading textColor={'white'}>Welcome to </Heading>
-                        <Heading textColor={'#CBE724'} ml={'0.5rem'}>Clarity</Heading>
-                        <Heading textColor={'white'}>.</Heading>
-                    </Flex>
-                    <Box maxW={'50vw'} textColor={'white'}>
-                        <Text>
+            <Box px={'9vw'}>
+                <Flex my={'5vh'} minH={'50vh'}>
+                    <Box w={'45vw'}>
+                        <Flex>
+                            <Heading textColor={'#C2C6CA'} fontWeight={'500'}>Welcome to</Heading>
+                            <Heading textColor={'#730295'} fontWeight={'500'} pl={'0.5rem'}>Clarity</Heading>
+                            <Heading textColor={'#C2C6CA'}>.</Heading>
+                        </Flex>
+                        <Text textColor={'#5E6172'} mt={'10px'}>
                             Clarity is made to help you shape your ideas and bring them to life.
+                        </Text>
+                        <Text textColor={'#5E6172'}>
                             Gain unmatched perspicacity by structuring your tasks and constantly analyzing your productivity.
                         </Text>
+                        <Button variant={'solid'} bgColor={'#730295'} rounded={'0.5rem'} mt={'30px'} p={'22px 40px'}
+                            _hover={{ bgColor: '#9100BD' }}
+                        >
+                            <Link href={'/register/'} style={{ textDecoration: 'none' }}>
+                                <Heading fontSize={'1.25rem'} textColor={'#C2C6CA'} textTransform={'uppercase'}>Try it</Heading>
+                            </Link>
+                        </Button>
                     </Box>
-                </VStack> */}
-                {/* <VStack w={'30vw'} rounded={'1rem'} minH={'50vh'} align={'start'} backdropFilter={'auto'} backdropBlur={'10px'}>
-                    <Heading p={'10px 20px 0px'} textColor={'white'}>Brand new paradigm.</Heading>
-                    <Text px={'20px'} pb={'30px'} textColor={'white'}>We offer a unique system with many powerful tools to maximise your progress.</Text>
-                    <Flex bgColor={'#F6AA8E'} w={'16vw'} h={'3rem'} roundedRight={'full'}
-                          _hover={{ width: '25vw', transition: 'ease .5s' }}
-                          transition={'width .5s ease'}
-                         border={'2px solid black'}
-                         cursor={'pointer'}
-                         borderLeft={'none'}
-                         pl={'20px'}
-                         pr={'30px'}
-                    >
-                        <Center>
-                            <Heading fontSize={'1.25rem'} fontWeight={'700'}>Fully customizable</Heading>
-                        </Center>
+                    <Box>
+                        <motion.svg viewBox={'0 0 400 300'}>
+                            <motion.path
+                                stroke={'#730295'}
+                                fill={'#730295'}
+                                d={'M 0 0 L 400 0 L 400 300 L 0 300 Z'}
+                                />
+                        </motion.svg>
+                    </Box>
+                </Flex>
+                <Flex w={'82vw'} boxShadow={'0px 0px 30px 10px rgba(0, 0, 0, .2)'} bgColor={'rgba(0, 0, 0, .2)'} rounded={'0.5rem'}>
+                    <Box textAlign={'center'} p={'10px'} bgColor={'#1C1F27'} rounded={'0.5rem'} w={'26vw'}>
+                        <Heading textColor={'#C2C6CA'} fontSize={'1.5rem'} fontWeight={'500'}
+                            >Fully customizable</Heading>
+                    </Box>
+                    <Box textAlign={'center'} p={'10px'} bgColor={'#1C1F27'} mx={'2vw'} rounded={'0.5rem'} w={'26vw'}>
+                        <Heading textColor={'#C2C6CA'} fontSize={'1.5rem'} fontWeight={'500'}
+                            >Task linking</Heading>
+                    </Box>
+                    <Box textAlign={'center'} p={'10px'} bgColor={'#1C1F27'} rounded={'0.5rem'} w={'26vw'}>
+                        <Heading textColor={'#C2C6CA'} fontSize={'1.5rem'} fontWeight={'500'}
+                        >Great visualization</Heading>
+                    </Box>
+                </Flex>
+                <Box w={'82vw'} textAlign={'center'} mt={'8vh'} p={'0!important'}>
+                    <Flex w={'fit-content'}>
+                        <Heading textColor={'#730295'} fontWeight={'500'}>Brand new</Heading>
+                        <Heading textColor={'#C2C6CA'} fontWeight={'500'} ml={'0.5rem'}>paradigm.</Heading>
                     </Flex>
-                    <Flex bgColor={'#F17F55'} w={'12vw'} h={'3rem'} roundedRight={'full'}
-                          _hover={{ width: '25vw', transition: 'ease .5s' }}
-                          transition={'width .5s ease'}
-                          border={'2px solid black'}
-                          cursor={'pointer'}
-                          borderLeft={'none'}
-                          pl={'20px'}
-                          pr={'30px'}
-                    >
-                        <Center>
-                            <Heading fontSize={'1.25rem'} fontWeight={'700'}>Task linking</Heading>
-                        </Center>
-                    </Flex>
-                    <Flex bgColor={'#ED541D'} width={'17vw'} h={'3rem'} roundedRight={'full'}
-                          _hover={{ width: '25vw', transition: 'ease .5s' }}
-                          transition={'width .5s ease'}
-                          border={'2px solid black'}
-                          cursor={'pointer'}
-                          borderLeft={'none'}
-                          pl={'20px'}
-                          pr={'30px'}
-                    >
-                        <Center>
-                            <Heading fontSize={'1.25rem'} fontWeight={'700'}>Great visualization</Heading>
-                        </Center>
-                    </Flex>
-                </VStack> */}
-            {/* </Flex> */}
-            {/* <IndexLinksBar /> */}
+                    <Text textColor={'#5E6172'} mt={'5px'}>
+                        We offer a unique system with many powerful tools to maximise your progress.
+                    </Text>
+                </Box>
+            </Box>
         </Box>
     );
 }
--- todo_frontend/src/components/Navbar.tsx
+++ todo_frontend/src/components/Navbar.tsx
@@ -8,24 +8,24 @@ function Navbar() {
             <Flex justify={'space-between'} w={'100vw'} p={'14px 3vw'}>
                 <Button variant={'unstyled'} _hover={{ bgColor: 'none' }} rounded={'full'}>
                         <Link href={'/'} style={{ textDecoration: 'none' }}>
-                            <Heading fontSize={'2rem'} fontWeight='500' textColor={'#C2C6CA'}>Clarity</Heading>
+                            <Heading fontSize={'2rem'} fontWeight='800' textColor={'#C2C6CA'}>Clarity</Heading>
                         </Link>
                 </Button>
                 <Flex>
                     <ButtonGroup gap={'10px'}>
-                        <Button variant={'ghost'} _hover={{ bgColor: 'rgba(78, 71, 89, .5)' }} rounded={'full'}>
+                        <Button variant={'ghost'} _hover={{ bgColor: 'rgba(78, 71, 89, .3)' }} rounded={'full'}>
                             <Link href={'/'} style={{ textDecoration: 'none' }}>
-                                <Text fontSize={'0.9rem'} textColor={'#C2C6CA'} textTransform={'uppercase'}>About</Text>
+                                <Text fontSize={'0.9rem'} textColor={'#5E6172'} textTransform={'uppercase'}>About</Text>
                             </Link>
                         </Button>
-                        <Button variant={'ghost'} _hover={{ bgColor: 'rgba(78, 71, 89, .5)' }} rounded={'full'}>
+                        <Button variant={'ghost'} _hover={{ bgColor: 'rgba(78, 71, 89, .3)' }} rounded={'full'}>
                             <Link href={'/'} style={{ textDecoration: 'none' }}>
-                                <Text fontSize={'0.9rem'} textColor={'#C2C6CA'} textTransform={'uppercase'}>Log out</Text>
+                                <Text fontSize={'0.9rem'} textColor={'#5E6172'} textTransform={'uppercase'}>Log out</Text>
                             </Link>
                         </Button>
-                        <Button variant={'ghost'} _hover={{ bgColor: 'rgba(78, 71, 89, .5)' }} rounded={'full'}>
+                        <Button variant={'ghost'} _hover={{ bgColor: 'rgba(78, 71, 89, .3)' }} rounded={'full'}>
                             <Link href={'/'} style={{ textDecoration: 'none' }}>
-                                <Text fontSize={'0.9rem'} textColor={'#C2C6CA'} textTransform={'uppercase'}>My account</Text>
+                                <Text fontSize={'0.9rem'} textColor={'#5E6172'} textTransform={'uppercase'}>My account</Text>
                             </Link>
                         </Button>
                     </ButtonGroup>
